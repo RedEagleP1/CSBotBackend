2271dde0b299eacec1154d044b03eb97232c3edc
Megafont <51346737+Megafont@users.noreply.github.com>
2024-09-15 13:17:42 -0500

    Quick fix

--- P1-Core/IRepository.cs
+++ P1-Core/IRepository.cs
@@ -6,7 +6,7 @@ namespace P1_Core
     public interface IRepository<TEntity> where TEntity : BaseEntity
     {
         Task<int> AddAsync(TEntity entity);
-        Task DeleteAsync(TEntity entity);
+        Task DeleteAsync(int id);
         Task UpdateAsync(TEntity entity);
         Task<TEntity> GetByIdAsync(int id);
         Task<IEnumerable<TEntity>> GetAllAsync();
